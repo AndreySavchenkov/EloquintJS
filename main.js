a6c48ebc8856cf85a288277f18af5c9ec3bfc1f9
Andrey <sannndvik16@gmail.com>
2020-01-03 21:18:57 +0300

    functions range and sum

--- main.js
+++ main.js
@@ -87,3 +87,24 @@ for(let y = 0; y < size; y++) {
 }
 
 console.log(board);
+
+
+//сумма диапазона
+function range(start, end, step = start < end ? 1 : -1) {
+  let array = [];
+
+  if (step > 0) {
+    for (let i = start; i <= end; i += step) array.push(i);
+  } else {
+    for (let i = start; i >= end; i += step) array.push(i);
+  }
+  return array;
+}
+
+function sum(array) {
+  let total = 0;
+  for (let value of array) {
+    total += value;
+  }
+  return total;
+}
